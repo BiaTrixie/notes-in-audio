5503470c7cd1297cf029c733d618a2a62186359c
BiaTrixie <bmoura276@gmail.com>
2024-02-14 16:50:50 -0300

    funcionando speech

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './assets/Logo.svg'
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card';
 
-interface Note{
+interface Note {
   id: string
   date: Date
   content: string
@@ -13,46 +13,55 @@ export function App() {
   const [search, setSearch] = useState('')
   const [notes, setNotes] = useState<Note[]>(() => {
     const notesOnStorage = localStorage.getItem('notes')
-    if(notesOnStorage){
+    if (notesOnStorage) {
       return JSON.parse(notesOnStorage)
     }
   })
 
-  function onNoteCreated(content: string){
-    const newNote ={
+  function onNoteCreated(content: string) {
+    const newNote = {
       id: crypto.randomUUID(),
       date: new Date(),
       content
     }
 
-    const notesArray = [newNote,...notes]
+    const notesArray = [newNote, ...notes]
 
     setNotes(notesArray)
 
     localStorage.setItem('notes', JSON.stringify(notes))
   }
 
-  function handleSearch (event: ChangeEvent<HTMLInputElement>){
-    const query = event.target.value 
+  function onNoteDeleted(id: string ){
+    const notesArray = notes.filter(note => {
+      return note.id != id
+    })
+
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
 
     setSearch(query)
   }
 
   const filteredNotes = search != '' ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
-: notes
+    : notes
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5 ">
       <img src={logo} alt="NWL Expert" />
       <form className="w-full">
-        <input type="text" placeholder='Busque em suas notas...'onChange={handleSearch} className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500" />
+        <input type="text" placeholder='Busque em suas notas...' onChange={handleSearch} className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500" />
       </form>
       <div className='h-px bg-slate-700' ></div>
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
 
-            <NewNoteCard onNoteCreated={onNoteCreated}  />
-      {filteredNotes.map(note =>{
-        return<NoteCard key={note.id} note={note}/>
-      })}
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {filteredNotes.map(note => {
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
+        })}
       </div>
     </div>
 
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -12,6 +12,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [content, setContent] = useState('')
   const [isRecording, setIsRecording] = useState(false)
 
+  let speechRecognition: SpeechRecognition | null = null
 
   function handleStartEditor() {
     setShouldshowOnboarding(false)
@@ -53,7 +54,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
     const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition
 
-    const speechRecognition = new SpeechRecognitionAPI()
+    speechRecognition = new SpeechRecognitionAPI()
 
     speechRecognition.lang = 'pt-BR'
     speechRecognition.continuous = true
@@ -61,7 +62,11 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     speechRecognition.interimResults = true
 
     speechRecognition.onresult = (event) => {
-      console.log(event.results)
+      const transcription = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript)
+      }, '')
+
+      setContent(transcription)
     }
 
     speechRecognition.onerror = (event) => {
@@ -73,6 +78,10 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
   function handleStopRecording() {
     setIsRecording(false)
+
+    if(speechRecognition != null){
+      speechRecognition.stop()
+    }
   }
 
   return (
@@ -84,7 +93,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className='inset-0 fixed bg-black/50'></Dialog.Overlay>
-        <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w[640pz] w-full h-[60vh] bg-slate-700 rounded=md flex flex-col outline-none'>
+        <Dialog.Content className='fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w[640pz] w-full md:h-[60vh] bg-slate-700 md:rounded=md flex flex-col outline-none'>
           <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
             <X className='size-5' />
           </Dialog.Close>
